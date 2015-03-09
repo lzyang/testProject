29e516bd64d105f6b6b444b9330d7a1c7e71bd14
lzyang <morningsun2218@163.com>
2015-03-09 15:22:54 +0800

    geoIndex

--- src/test/java/es/ESIndex.java
+++ src/test/java/es/ESIndex.java
@@ -1,7 +1,43 @@
 package es;
 
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+
+import java.util.Random;
+
 /**
  * Created by root on 15-1-30.
  */
 public class ESIndex {
+
+    public void indexData(BasicDBList docs){
+        Client client = ESClientUtils.getTranClient("127.0.0.1", 9300, "lzyESTest");
+        BulkRequestBuilder bulkReq = client.prepareBulk();
+        try{
+            for(int i=0;i<docs.size();i++){
+                BasicDBObject o = (BasicDBObject)docs.get(i);
+
+                XContentBuilder doc = XContentFactory.jsonBuilder()
+                        .startObject()
+                        .field("datatime", o.getLong("startDate", 0))
+                        .field("id", o.getString("productId", ""))
+                        .field("num",new Random().nextInt(5)+1)
+                        .field("pname", o.getString("title", ""))
+                        .field("price", o.getDouble("price", 0.00))
+                        .field("userid", o.getString("skuNo", "").substring(0, 7))
+                        .endObject();
+
+                bulkReq.add(client.prepareIndex("order_v1", "orderType",o.getString("skuId"))
+                                .setSource(doc)
+                );
+            }
+            bulkReq.execute().actionGet();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
