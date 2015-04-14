cdc5a242013727e57a27a79d13f80a7146151e2c
lzyang <morningsun2218@163.com>
2015-04-14 16:45:35 +0800

    read file

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -2,6 +2,11 @@ import com.mongodb.BasicDBObject;
 import io.netty.buffer.UnpooledHeapByteBuf;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 /**
  * Created by root on 15-2-27.
  */
@@ -17,6 +22,20 @@ public class MyTest {
 
     @Test
     public void testBuf(){
-        
+
+    }
+
+    @Test
+    public void convTime(){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sdf.format(new Date(1427359588502l)));//1423106324413
+        System.out.println(sdf.format(new Date(1426238370913l)));//1423106324413
+
+        try {
+            Date d = sdf.parse("2015-03-13 17:00:00");
+            System.out.println(d.getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
