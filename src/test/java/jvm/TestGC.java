a7409e3ef27824bd783903c2d0f805d9e7950845
lzyang <morningsun2218@163.com>
2015-06-02 18:23:42 +0800

    GC Test

--- src/test/java/jvm/TestGC.java
+++ src/test/java/jvm/TestGC.java
@@ -1,6 +1,9 @@
 package jvm;
 
+import com.sun.corba.se.impl.encoding.CodeSetConversion;
 import org.junit.Test;
+
+import java.util.Vector;
 
 /**
  * Created by root on 15-6-1.
@@ -11,10 +14,29 @@ public class TestGC {
      * -ea -Xms100m -Xmx100m -Xmn40m -XX:+PrintGCDetails -XX:PermSize=3m -XX:MaxPermSize=3m
      */
     @Test
-    public void testPermGC(){
-        for(int i=0;i<Long.MAX_VALUE;i++){
+    public void testPermGC() {
+        for (int i = 0; i < Long.MAX_VALUE; i++) {
             String test = String.valueOf(i).intern();
         }
     }
 
+    /**
+     * -ea -Xms100m -Xmx100m -Xmn40m -XX:+PrintGCDetails
+     */
+    @Test
+    public void testXmx() {
+        try {
+            Vector<byte[]> cache = new Vector<byte[]>();
+            for (int i = 1; i < Long.MAX_VALUE; i++) {
+                byte[] a = new byte[1024 * 1024];
+                cache.add(a);
+                System.out.println("cache" + i + "M");
+                Thread.sleep(100);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Max memory:" + Runtime.getRuntime().maxMemory() / 1024 / 1024 + "M.");
+        }
+    }
 }
