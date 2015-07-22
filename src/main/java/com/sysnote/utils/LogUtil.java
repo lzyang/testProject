355d613d52445e31ae6f12ab52e50bb3b0826986
lzyang <morningsun2218@163.com>
2015-07-22 14:27:20 +0800

    test log

---
+++ src/main/java/com/sysnote/utils/LogUtil.java
@@ -0,0 +1,19 @@
+package com.sysnote.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Created by Morningsun(515190653@qq.com) on 15-7-22.
+ */
+public class LogUtil {
+
+    private static Logger testLog = LoggerFactory.getLogger("TestLog");
+
+    public static void printLog(String content){
+        StringBuffer sbf = new StringBuffer();
+        sbf.append("[").append(StringUtil.currentFullTime()).append("] ").append(content);
+        testLog.info(sbf.toString());
+    }
+}
