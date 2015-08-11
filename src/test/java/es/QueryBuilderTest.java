6e0aa084dfb51f9cb42164a268910c0a861dbdb6
lzyang <morningsun2218@163.com>
2015-08-11 19:13:15 +0800

    ES Test

--- src/test/java/es/QueryBuilderTest.java
+++ src/test/java/es/QueryBuilderTest.java
@@ -2,11 +2,13 @@ package es;
 
 import groovy.sql.Sql;
 import org.elasticsearch.common.lucene.search.function.FieldValueFactorFunction;
+import org.elasticsearch.common.lucene.search.function.ScoreFunction;
 import org.elasticsearch.index.query.BoolFilterBuilder;
 import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
+import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilder;
 import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
 import org.junit.Test;
 
@@ -23,6 +25,10 @@ public class QueryBuilderTest {
                 ScoreFunctionBuilders.fieldValueFactorFunction("servscore").modifier(FieldValueFactorFunction.Modifier.LOG1P).factor(0.1f))
         .boostMode("sum")
         .maxBoost(1.5f);
+
+
+        query = QueryBuilders.functionScoreQuery(FilterBuilders.termFilter("city","name"), ScoreFunctionBuilders.weightFactorFunction(0.5f));
+
 
         System.out.println(query);
     }
