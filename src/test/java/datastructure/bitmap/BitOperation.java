8e389611fb328776b63786b35e52cc55f036f2e1
lzyang <morningsun2218@163.com>
2015-08-14 13:02:49 +0800

    ES Test

---
+++ src/test/java/datastructure/bitmap/BitOperation.java
@@ -0,0 +1,85 @@
+package datastructure.bitmap;
+
+import org.junit.Test;
+
+/**
+ * Created by Morningsun(515190653@qq.com) on 15-8-14.
+ */
+public class BitOperation {
+
+    /**
+     * 清零操作
+     */
+    @Test
+    public void clear(){
+        int a = 852;
+        //对数字a和十六进制0去且操作
+        int b = a&0x0;
+        System.out.println(b);
+        printBinary(65535);
+    }
+
+    /**
+     *
+     * @param x
+     */
+    public void printBinary(int x){
+        StringBuffer binaryStr = new StringBuffer("");
+        for(int i = 0;i<31;i++){
+            if((x & (1 << i))>0){
+                binaryStr.append(1);
+            }else{
+                binaryStr.append(0);
+            }
+        }
+        System.out.println(binaryStr);
+    }
+
+    public void printBinary(long x){
+        StringBuffer binaryStr = new StringBuffer("");
+        for(int i = 0;i<63;i++){
+            if((x & (1 << i))>0){
+                binaryStr.append(1);
+            }else{
+                binaryStr.append(0);
+            }
+        }
+        System.out.println(binaryStr);
+    }
+
+    public void printBinary(short x){
+        StringBuffer binaryStr = new StringBuffer("");
+        for(int i = 0;i<15;i++){
+            if((x & (1 << i))>0){
+                binaryStr.append(1);
+            }else{
+                binaryStr.append(0);
+            }
+        }
+        System.out.println(binaryStr);
+    }
+
+    public void printBinary(char x){
+        StringBuffer binaryStr = new StringBuffer("");
+        for(int i = 0;i<15;i++){
+            if((x & (1 << i))>0){
+                binaryStr.append(1);
+            }else{
+                binaryStr.append(0);
+            }
+        }
+        System.out.println(binaryStr);
+    }
+
+    public void printBinary(byte x){
+        StringBuffer binaryStr = new StringBuffer("");
+        for(int i = 0;i<7;i++){
+            if((x & (1 << i))>0){
+                binaryStr.append(1);
+            }else{
+                binaryStr.append(0);
+            }
+        }
+        System.out.println(binaryStr);
+    }
+}
