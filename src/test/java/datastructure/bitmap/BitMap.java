927a3f3903df3bb6cd5fc57a82fcc06af2c274fb
lzyang <morningsun2218@163.com>
2015-08-26 17:33:05 +0800

    bitMap update

--- src/test/java/datastructure/bitmap/BitMap.java
+++ src/test/java/datastructure/bitmap/BitMap.java
@@ -18,12 +18,14 @@ public class BitMap {
     BitMap(int storeSize) {
         storeCellCount = storeSize / 31 + 1;
         bitMap = new Vector<Integer>(storeCellCount);
+        bitMap.setSize(storeCellCount);
         maxBitCount = storeCellCount*31;
         clear();
+
     }
 
     public void clear(){
-        for (int i = 0; i < bitMap.size(); i++)
+        for (int i = 0; i < storeCellCount; i++)
             bitMap.set(i, 0);
     }
 
@@ -60,19 +62,77 @@ public class BitMap {
 
         int cellPos = bitPosition/31;
         int PosInCell = bitPosition%31;
+        //System.out.println("cellPos:"+cellPos + " PosInCell"+PosInCell + "  bitSize:"+bitMap.size());
         int cell = bitMap.elementAt(cellPos);
         if(bit == true){
             cell |= 1<<PosInCell;
         }else{
             cell &= (1<<PosInCell)^Integer.MAX_VALUE;
         }
+        bitMap.set(cellPos,cell);
     }
 
     /**
      * 获取bit位
      */
-    public void getBit(){
+    public boolean getBit(int pos){
+        if(pos<1||pos>maxBitCount){
+            try {
+                throw new Exception("parameter must be positive and can't pass MaxBitCount!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+        int cellPos = pos/31;
+        int PosInCell = pos%31;
+        int cell = bitMap.elementAt(cellPos);
+        if((cell&(1<<PosInCell))>0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
+    public void printBinary(int x){
+        StringBuffer binaryStr = new StringBuffer("");
+        for(int i = 0;i<31;i++){
+            if((x & (1 << i))>0){
+                binaryStr.append(1);
+            }else{
+                binaryStr.append(0);
+            }
+        }
+        System.out.println(binaryStr);
+    }
+
+    public static void main(String[] args){
+        BitMap bm = new BitMap(3880);
+        bm.setBit(3880,true);
+        bm.setBit(9,true);
+        bm.setBit(8,true);
+        bm.setBit(7,true);
+        bm.setBit(6,true);
+        bm.setBit(1,true);
+        System.out.println("3880>"+bm.getBit(3880));
+        System.out.println("9>"+bm.getBit(9));
+        System.out.println("8>"+bm.getBit(8));
+        System.out.println("7>"+bm.getBit(7));
+        System.out.println("6>"+bm.getBit(6));
+        System.out.println("5>"+bm.getBit(5));
+        System.out.println("1>"+bm.getBit(1));
+
+        System.out.println("============================");
+        bm.setBit(1,false);
+        bm.setBit(7, false);
+        bm.setBit(3880,false);
+        System.out.println("3880>" + bm.getBit(3880));
+        System.out.println("9>" + bm.getBit(9));
+        System.out.println("8>"+bm.getBit(8));
+        System.out.println("7>"+bm.getBit(7));
+        System.out.println("6>"+bm.getBit(6));
+        System.out.println("5>"+bm.getBit(5));
+        System.out.println("1>"+bm.getBit(1));
     }
 }
 
