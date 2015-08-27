3a17e483329cb8002cf2c269fd6be6f086991473
lzyang <morningsun2218@163.com>
2015-08-27 19:03:10 +0800

    template set

--- src/test/java/es/ESIndex.java
+++ src/test/java/es/ESIndex.java
@@ -10,6 +10,7 @@ import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Random;
 
@@ -57,6 +58,23 @@ public class ESIndex {
             System.out.println("Order:"+temp.getOrder());
             System.out.println("Settings:"+temp.getSettings().getAsMap());
             System.out.println("Mappings:"+temp.getMappings());
+        }
+    }
+
+    @Test
+    public void templateSet(){
+        Client client = ESClientUtils.getTranClient("127.0.0.1", 9300, "lzyESTest");
+        try {
+            client.admin().indices().preparePutTemplate("template_1")
+                    .setTemplate("te*")
+                    .setOrder(0)
+                    .addMapping("type1", XContentFactory.jsonBuilder().startObject().startObject("type1").startObject("properties")
+                            .startObject("field1").field("type", "string").field("store", "yes").endObject()
+                            .startObject("field2").field("type", "string").field("store", "yes").field("index", "not_analyzed").endObject()
+                            .endObject().endObject().endObject())
+                    .get();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
