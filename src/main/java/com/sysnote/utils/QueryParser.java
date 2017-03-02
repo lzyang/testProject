a5463b3d7df9aae032a3af7f88d7df90820b2a5c
lzyang <morningsun2218@163.com>
2017-03-02 13:55:07 +0800

    current

--- src/main/java/com/sysnote/utils/QueryParser.java
+++ src/main/java/com/sysnote/utils/QueryParser.java
@@ -1,9 +1,11 @@
 package com.sysnote.utils;
 
 import org.ansj.domain.Term;
+import org.ansj.splitWord.analysis.IndexAnalysis;
 import org.ansj.splitWord.analysis.ToAnalysis;
 import org.nlpcn.commons.lang.dat.Item;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,18 +15,20 @@ import java.util.List;
  */
 public class QueryParser {
 
-    private String dealSpecial(String query){
-        return query.replaceAll("[)(-/]）（:"," ");
+    private static String dealSpecial(String query){
+        return query.replaceAll("[)(-/）（:【】，*！、《》]"," ");
     }
 
-    public String parse(String query){
+    public static String parse(String query){
         query = dealSpecial(query);
-        List<Term> parse = ToAnalysis.parse(query);
+//        List<Term> parse = ToAnalysis.parse(query);
+        List<Term> parse = IndexAnalysis.parse(query);
         Iterator<Term> it = parse.iterator();
         StringBuffer sb = new StringBuffer();
         int count = 0;
         while (it.hasNext()){
             Term t = it.next();
+            if(sb.indexOf(t.getName())>0) continue;
             if(count++>0)
                 sb.append(t.getName()).append(" ");
             else{
